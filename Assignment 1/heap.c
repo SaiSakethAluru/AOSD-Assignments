9f2dc3c9f4678ec7e0254aa5c8a3656f4c0c60e2
Sai Saketh Aluru <alurusaisaketh@gmail.com>
2020-09-23 16:47:42 +0530

    Modify heap.c and create makefile

--- Assignment 1/heap.c
+++ Assignment 1/heap.c
@@ -3,12 +3,17 @@
 #include <linux/proc_fs.h>
 #include <linux/uaccess.h>
 #include <linux/kernel.h>
+#include <linux/slab.h>
 
 MODULE_LICENSE("GPL");
 
+static ssize_t write(struct file *file, const char *buf, size_t count, loff_t *pos);
+static ssize_t read(struct file *file, char *buf, size_t count, loff_t *pos);
+static void heapify(int i);
+static int heap_pop(void);
+static void heap_insert(int value);
+
 static struct file_operations file_ops;
-// static char buffer[256] = {0}; 
-// static int buffer_len = 0;
 
 static int lchild(int i)
 {
@@ -25,7 +30,7 @@ static int parent(int i)
     return (i-1)/2;
 }
 
-static typedef struct heap{
+typedef struct heap{
     int type;
     int max_size;
     int size;
@@ -42,10 +47,10 @@ static int heap_init(void)
     heap_obj.size = 0;
     heap_obj.max_size = 0;
     heap_obj.init = 0;
-    heap_obj.elem = NULL;
+    heap_obj.arr = NULL;
     struct proc_dir_entry *entry = proc_create("partb_1_16CS30030", 0, NULL, &file_ops);
     if(!entry) return -ENOENT;
-    ile_ops.owner = THIS_MODULE; 
+    file_ops.owner = THIS_MODULE; 
     file_ops.write = write;
     file_ops.read = read;
     printk(KERN_ALERT "Initialisation of heap module done");
@@ -54,7 +59,7 @@ static int heap_init(void)
 static ssize_t write(struct file *file, const char *buf, size_t count, loff_t *pos) { 
     // printk("%.*s", count, buf);
     int value;
-    int valid = kstrtol(buf,0,&value);
+    int valid = kstrtoint(buf,0,&value);
     if(!valid) return -EINVAL;
     if(heap_obj.init==0){
         if(value!=0xFF && value!=0xF0){
@@ -80,7 +85,7 @@ static ssize_t write(struct file *file, const char *buf, size_t count, loff_t *p
         else{
             heap_obj.init = 2;
             heap_obj.max_size = value;
-            heap_obj.arr = (int*) kmalloc((heap_obj.max_size)*sizeof(int));
+            heap_obj.arr = (int*) kmalloc((heap_obj.max_size)*sizeof(int),GFP_KERNEL);
         }
         return count;
     }
@@ -100,7 +105,7 @@ static ssize_t read(struct file *file, char *buf, size_t count, loff_t *pos) {
         return -1;
     }
     if(heap_obj.size==0){
-        return -EACCESS;
+        return -EACCES;
     }
     int value = heap_pop();
     if(copy_to_user(buf,&value,sizeof(value))){
@@ -111,8 +116,8 @@ static ssize_t read(struct file *file, char *buf, size_t count, loff_t *pos) {
 static void heapify(int i)
 {
     if(heap_obj.type==1){
-        int smallest = (lchild(i) < heap_obj.size && heap_obj.arr[lchild(i)] < heap_obj.arr[i]? lchild(i) : i;
-        if(rchild(i) < heap_obj.size && heap_obj.arr[rchild(i)] < heap_obj.arr[smallest]{
+        int smallest = (lchild(i) < heap_obj.size && heap_obj.arr[lchild(i)] < heap_obj.arr[i])? lchild(i) : i;
+        if(rchild(i) < heap_obj.size && heap_obj.arr[rchild(i)] < heap_obj.arr[smallest]){
             smallest = rchild(i);
         }
         if(smallest != i){
@@ -123,7 +128,7 @@ static void heapify(int i)
         }
     }
     else if(heap_obj.type==2){
-        int largest = (lchild(i) < heap_obj.size && heap_obj.arr[lchild(i)] > heap_obj.arr[i]? lchild(i) : i;
+        int largest = (lchild(i) < heap_obj.size && heap_obj.arr[lchild(i)] > heap_obj.arr[i])? lchild(i) : i;
         if(rchild(i)< heap_obj.size && heap_obj.arr[rchild(i)] > heap_obj.arr[largest]){
             largest = rchild(i);
         }
@@ -136,7 +141,7 @@ static void heapify(int i)
     }
 }
 
-static int heap_pop()
+static int heap_pop(void)
 {
     heap_obj.size--;
     int root = heap_obj.arr[0];
